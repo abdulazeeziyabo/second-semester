c7190b4ea6f35c4e23db65afc633b7f182783471
Abdulazeez <bawallabolaji@gmail.com>
2023-10-22 19:18:36 +0100

    added home page

---
+++ src/views/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className='home-page'>
+      <h1>WELCOME TO MY GITHUB PROFILE</h1>
+      <a
+        href='https://github.com/abdulazeeziyabo?tab=repositories'
+        target='_blank'
+      >
+        <img
+          src='./public/github-mark.svg'
+          alt='GitHub Avatar'
+          className='github-name'
+        />
+      </a>
+      <Link to='repository' className='home-page-link'>View my repositories</Link>
+      <button className='btn-repo'>
+        <Link to='/error-testing' className='error-testing'>
+          Error Testing
+        </Link>
+      </button>
+      <p> &#169; AltSchool Second Semester Project 2023 <span>Created by Iyabo Abdulazeez</span></p>
+    </div>
+  );
+}
+
+export default Home
